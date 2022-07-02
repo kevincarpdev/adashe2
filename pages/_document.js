e03bae99565959511a519027293b82e7b58779e3
Kevin Carpenter <carpenter.kevin@gmail.com>
2022-07-02 15:21:39 -0700

    Adashe with Tailwind and logo images

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-      {/* Add Google fonts here */}
+          {/* Add Google fonts here */}
         </Head>
         <body>
           <Main />
