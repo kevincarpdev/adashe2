f485893b9514252e519106d00408d4d86e936da0
Kevin Carpenter <carpenter.kevin@gmail.com>
2022-07-03 16:14:24 -0700

    Other fixes

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* Add Google fonts here */}
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Poppins:wght@300;400;500&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
